650438c6d8d90b43276e65bab9745cde84894630
Patryk Grudniewski <patgrudniewski@gmail.com>
2017-04-20 09:48:22 +0200

    Move FieldToEntityTransformer to opstalent/api-bundle

---
+++ Form/DataTransformer/FieldToEntityTransformer.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Opstalent\ApiBundle\Form\DataTransformer;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\DataTransformerInterface;
+use Symfony\Component\Form\Exception\TransformationFailedException;
+
+/**
+ * Class FieldToEntityTransformer
+ * @package Opstalent\ApiBundle
+ */
+class FieldToEntityTransformer implements DataTransformerInterface
+{
+    private $manager;
+    private $field;
+    private $entityClass;
+
+    /**
+     * FieldToEntityTransformer constructor.
+     * @param EntityManager $manager
+     * @param string $field
+     * @param string $entityClass
+     */
+    public function __construct(EntityManager $manager, string $entityClass, string $field = "id")
+    {
+        $this->manager = $manager;
+        $this->field = $field;
+        $this->entityClass = $entityClass;
+    }
+
+
+    /**
+     * @param mixed $object
+     * @return string
+     */
+    public function transform($object)
+    {
+        if (null === $object) {
+            return '';
+        }
+
+        $getter = "get". ucfirst($this->field);
+        return $object->$getter();
+    }
+
+
+    /**
+     * @param mixed $fieldValue
+     * @return null|object
+     */
+    public function reverseTransform($fieldValue)
+    {
+        if (!$fieldValue) {
+            return null;
+        }
+        if(is_array($fieldValue)) return array_map([$this,'reverseTransform'], $fieldValue);
+        if($this->field === 'id') {
+            $object = $this->manager->getReference($this->entityClass, $fieldValue);
+        } else {
+            $object = $this->manager
+                ->getRepository($this->entityClass)
+                ->findOneBy([$this->field => $fieldValue]);
+        }
+
+
+        if (null === $object) {
+            throw new TransformationFailedException(sprintf(
+                "Data does not exist!",
+                $fieldValue
+            ));
+        }
+
+        return $object;
+    }
+}
