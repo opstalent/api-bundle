90c2e35cd4e026bf5afe57a5e722eeb01b9fa371
Patryk Grudniewski <patgrudniewski@gmail.com>
2017-04-25 15:42:08 +0200

    Apply annotations to ActionController

--- Controller/ActionController.php
+++ Controller/ActionController.php
@@ -2,24 +2,24 @@
 
 namespace Opstalent\ApiBundle\Controller;
 
+use Opstalent\ApiBundle\Annotation as API;
 use Opstalent\ApiBundle\Event\ApiEvent;
 use Opstalent\ApiBundle\Event\ApiEvents;
 use Opstalent\ApiBundle\Repository\BaseRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Form;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Exception\AccessDeniedException;
-use Symfony\Component\Form\AbstractType;
-use Nelmio\ApiDocBundle\Annotation\ApiDoc;
-use Symfony\Component\Routing\Route;
-use Symfony\Component\Security\Core\Role\Role;
 
+/**
+ * @API\Response(class="Symfony\Component\HttpFoundation\JsonResponse")
+ */
 class ActionController extends Controller
 {
 
+    /**
+     * @API\Serializable(method="list")
+     */
     public function listAction(Request $request)
     {
         $route = $this->get('router')->getRouteCollection()->get($request->attributes->get('_route'));
@@ -36,19 +36,16 @@ class ActionController extends Controller
         if (($form->isSubmitted() && $form->isValid()) || $form->isEmpty()) {
             /** @var BaseRepository $repository */
             $repository = $this->get(substr($route->getOption('repository'), 1));
-            return new Response(
-                $this->get('opstalent.api_bundle.serializer_service')->serialize(
-                    $repository->searchByFilters(is_array($form->getData()) ? $form->getData() : [])
-                    , "json", ['enable_max_depth' => true, 'groups' => $this->get('opstalent.api_bundle.serializer_service')->generateSerializationGroup($route, "list")]
-                ),
-                200,
-                ['Content-Type' => 'application/json']
-            );
+
+            return $repository->searchByFilters(is_array($form->getData()) ? $form->getData() : []);
         } else {
             throw new \Exception((string)$form->getErrors(true, false), 400);
         }
     }
 
+    /**
+     * @API\Serializable(method="get")
+     */
     public function getAction(Request $request, int $id)
     {
         $route = $this->get('router')->getRouteCollection()->get($request->attributes->get('_route'));
@@ -56,17 +53,13 @@ class ActionController extends Controller
         $repository = $this->get(substr($route->getOption('repository'), 1));
         $data = $repository->find($id);
         if ($data) {
-            return new Response(
-                $this->get('opstalent.api_bundle.serializer_service')->serialize(
-                    $data
-                    , "json", ['enable_max_depth' => true, 'groups' => $this->get('opstalent.api_bundle.serializer_service')->generateSerializationGroup($route, "get", $data)]
-                ),
-                200,
-                ['Content-Type' => 'application/json']
-            );
+            return $data;
         } else throw new \Exception("Not Found", 404);
     }
 
+    /**
+     * @API\Serializable(method="get")
+     */
     public function postAction(Request $request)
     {
         $route = $this->get('router')->getRouteCollection()->get($request->attributes->get('_route'));
@@ -76,19 +69,16 @@ class ActionController extends Controller
         if (($form->isSubmitted() && $form->isValid())) {
             /** @var BaseRepository $repository */
             $repository = $this->get(substr($route->getOption('repository'), 1));
-            return new Response(
-                $this->get('opstalent.api_bundle.serializer_service')->serialize(
-                    $repository->persist($form->getData(), true)
-                    , "json", ['enable_max_depth' => true, 'groups' => $this->get('opstalent.api_bundle.serializer_service')->generateSerializationGroup($route, "get", $form->getData())]
-                ),
-                200,
-                ['Content-Type' => 'application/json']
-            );
+
+            return $repository->persist($form->getData(), true);
         } else {
             throw new \Exception((string)$form->getErrors(true, true), 400);
         }
     }
 
+    /**
+     * @API\Serializable(method="get")
+     */
     public function putAction(Request $request, int $id)
     {
         $route = $this->get('router')->getRouteCollection()->get($request->attributes->get('_route'));
@@ -106,15 +96,7 @@ class ActionController extends Controller
             $form->handleRequest($request);
             $this->get('event_dispatcher')->dispatch(ApiEvents::POST_HANDLE_REQUEST, new ApiEvent($request, $form));
             if ($form->isSubmitted() && $form->isValid()) {
-
-                return new Response(
-                    $this->get('opstalent.api_bundle.serializer_service')->serialize(
-                        $repository->persist($form->getData(), true)
-                        , "json", ['enable_max_depth' => true, 'groups' => $this->get('opstalent.api_bundle.serializer_service')->generateSerializationGroup($route, "get", $form->getData())]
-                    ),
-                    200,
-                    ['Content-Type' => 'application/json']
-                );
+                return $repository->persist($form->getData(), true);
             } else {
                 throw new \Exception((string)$form->getErrors(true, false), 404);
             }
@@ -122,20 +104,16 @@ class ActionController extends Controller
         } else throw new \Exception("Not Found", 404);
     }
 
+    /**
+     * @API\Serializable(method="get")
+     */
     public function deleteAction(Request $request, int $id)
     {
         $route = $this->get('router')->getRouteCollection()->get($request->attributes->get('_route'));
         /** @var BaseRepository $repository */
         $repository = $this->get(substr($route->getOption('repository'), 1));
         if ($entity = $repository->find($id)) {
-            return new Response(
-                $this->get('opstalent.api_bundle.serializer_service')->serialize(
-                    $repository->remove($entity, true, $request->request->all())
-                    , "json", ['enable_max_depth' => true, 'groups' => $this->get('opstalent.api_bundle.serializer_service')->generateSerializationGroup($route, "get", $entity)]
-                ),
-                200,
-                ['Content-Type' => 'application/json']
-            );
+            return $repository->remove($entity, true, $request->request->all());
         } else throw new \Exception("Not Found", 404);
     }
 
