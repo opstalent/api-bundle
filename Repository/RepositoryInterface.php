d46f78e0bd001ab1ffd37ba82562726030d9eb24
Patryk Grudniewski <patgrudniewski@gmail.com>
2017-05-05 10:48:22 +0200

    Repository interface

---
+++ Repository/RepositoryInterface.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Opstalent\ApiBundle\Repository;
+
+/**
+ * @author Patryk Grudniewski <patgrudniewski@gmail.com>
+ */
+interface RepositoryInterface
+{
+    /**
+     * @param int $limit
+     */
+    public function setLimit(int $limit);
+
+    /**
+     * @param int $offset
+     */
+    public function setOffset(int $offset);
+
+    /**
+     * @param string $order
+     * @param string $orderBy
+     */
+    public function setOrder(string $order, string $orderBy);
+
+    /**
+     * @param array $filters
+     * @return array
+     */
+    public function searchByFilters(array $data) : array;
+}
